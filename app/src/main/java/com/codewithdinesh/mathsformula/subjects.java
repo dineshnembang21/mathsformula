874777b20462633a34e433eff85d784618be250d
Dinesh nembang <dineshnembang98@gmail.com>
2021-04-03 19:34:49 +0545

    third commit

--- app/src/main/java/com/codewithdinesh/mathsformula/subjects.java
+++ app/src/main/java/com/codewithdinesh/mathsformula/subjects.java
@@ -2,25 +2,25 @@ package com.codewithdinesh.mathsformula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class grade extends AppCompatActivity {
-Button btn_grade9;
+public class subjects extends AppCompatActivity {
+   Button btn_sub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grade);
-        btn_grade9=(Button) findViewById(R.id.grade9);
+        setContentView(R.layout.activity_subjects);
 
-        btn_grade9.setOnClickListener(new View.OnClickListener() {
+        btn_sub=(Button) findViewById(R.id.subject1);
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in=new Intent(grade.this,subjects.class);
-                 startActivity(in);
+                Intent in=new Intent(subjects.this,index.class);
+                startActivity(in);
 
             }
         });
